9cbb9bb2b1384465b95c8a463f348f64825eb18d
Sam Corcoran <samuel.corcoran@gmail.com>
2018-09-18 22:07:17 +0100

    Cells track perimeter nodes and neighbour cells and can sort them by bearing

--- evere++/SpatialCell.cpp
+++ evere++/SpatialCell.cpp
@@ -1,14 +1,58 @@
 #include <memory>
 #include <vector>
+#include <utility>	// std::pair
+#include <algorithm>	// std::sort
 
 #include "SpatialCell.h"
 #include "Point.h"
+#include "GeometryUtils.h"
 
 using namespace std;
 
 SpatialCell::SpatialCell(std::shared_ptr<Point> centre) : centre(centre) {
 };
 
-void SpatialCell::add_perimeter_point(std::shared_ptr<Point> p) {
-	perimeter.emplace(p);
+void SpatialCell::add_perimeter_node(std::shared_ptr<SpatialNode> n) {
+	perimeter.emplace(n);
 };
+
+void SpatialCell::add_neighbour_cell(std::shared_ptr<SpatialCell> c) {
+	neighbours.emplace(c);
+}
+
+void SpatialCell::sort_perimeter_by_bearing() {
+	// Calculate bearings to each perimeter node
+	vector<pair<shared_ptr<SpatialNode>,float>> perimeter_bearings(perimeter.size());
+	for (auto& node : perimeter) {
+		perimeter_bearings.emplace_back(make_pair(node,
+								GeometryUtils::initial_bearing(*centre, *node->location)));
+	}
+	// Sort pairs based on bearing values
+	sort(perimeter_bearings.begin(), perimeter_bearings.end(), [](auto &left, auto &right) {
+		return left.second < right.second;
+	});
+	// Update perimeter with new ordering
+	perimeter.clear();
+	for (auto& p : perimeter_bearings) {
+		perimeter.emplace(p.first);
+	}
+}
+
+void SpatialCell::sort_neighbours_by_bearing() {
+	// Calculate bearings to each neighbouring cell
+	vector<pair<shared_ptr<SpatialCell>, float>> neighbour_bearings(perimeter.size());
+	for (auto& cell : neighbours) {
+		neighbour_bearings.emplace_back(make_pair(cell,
+			GeometryUtils::initial_bearing(*centre, *cell->centre)));
+	}
+	// Sort pairs based on bearing values
+	sort(neighbour_bearings.begin(), neighbour_bearings.end(), [](auto &left, auto &right) {
+		return left.second < right.second;
+	});
+	// Update neighbours set with new ordering
+	neighbours.clear();
+	for (auto& p : neighbour_bearings) {
+		neighbours.emplace(p.first);
+	}
+
+}
--- evere++/SpatialCell.h
+++ evere++/SpatialCell.h
@@ -5,15 +5,23 @@
 #include <set>
 
 #include "Point.h"
+#include "SpatialNode.h"
+
+class SpatialNode;
 
 class SpatialCell {
 public:
 	SpatialCell(std::shared_ptr<Point> p);
 
 	std::shared_ptr<Point> centre;
-	std::set<std::shared_ptr<Point>> perimeter;
+	std::set<std::shared_ptr<SpatialNode>> perimeter;
+	std::set<std::shared_ptr<SpatialCell>> neighbours;
 
-	void add_perimeter_point(std::shared_ptr<Point> p);
+	void add_perimeter_node(std::shared_ptr<SpatialNode> n);
+	void add_neighbour_cell(std::shared_ptr<SpatialCell> c);
+
+	void sort_perimeter_by_bearing();
+	void sort_neighbours_by_bearing();
 private:
 };
 
