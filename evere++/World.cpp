a5f9455dbec35d764a47db3b00a518f00d76d463
Sam Corcoran <samuel.corcoran@gmail.com>
2018-09-19 00:50:47 +0100

    Nodegraph relation creation within World using triangles

--- evere++/World.cpp
+++ evere++/World.cpp
@@ -19,12 +19,75 @@ World::World(int const subdivisions) {
 };
 
 void World::construct_nodes(vector<unique_ptr<Triangle>> const & triangles) {
-	for (auto& t : triangles)
-	{
-		// TODO:
-		// Create cell from each point
-		// Create spatial node from triangle centre
-		// Add node to perimeter of each cell
-		// Add cell to 'converging' list for each node
+
+	map<Point const * const, SpatialCell * const> known_cells;
+	map<Triangle const * const, SpatialNode * const> known_nodes;
+
+	for (auto& t : triangles) {
+		// TODO: Finish nodegraph creation
+
+		// Each triangle-point represents a cell centre
+		shared_ptr<SpatialCell> cell_one = retrieve_or_create_cell(known_cells, t->p1);
+		shared_ptr<SpatialCell> cell_two = retrieve_or_create_cell(known_cells, t->p2);
+		shared_ptr<SpatialCell> cell_three = retrieve_or_create_cell(known_cells, t->p3);
+
+		// Each triangle-centre is a boundary point
+		shared_ptr<SpatialNode> node = retrieve_or_create_node(known_nodes, t.get());
+
+		// Cells must know about this perimeter node
+		cell_one->add_perimeter_node(node);
+		cell_two->add_perimeter_node(node);
+		cell_three->add_perimeter_node(node);
+
+		// Node must know about cells that surround it
+		node->add_surrounding_cell(cell_one);
+		node->add_surrounding_cell(cell_two);
+		node->add_surrounding_cell(cell_three);
+
+		// Cells must know about each other as neighbours
+		cell_one->add_neighbour_cell(cell_two);
+		cell_one->add_neighbour_cell(cell_three);
+		cell_two->add_neighbour_cell(cell_one);
+		cell_two->add_neighbour_cell(cell_three);
+		cell_three->add_neighbour_cell(cell_one);
+		cell_three->add_neighbour_cell(cell_two);
+	}
+
+	// Nodes must know about each other as neighbours
+	for (auto& cell : known_cells) {
+		// Sort their perimeter points by bearing, adding consecutive nodes as connections
+		cell.second->sort_and_connect_perimeter_nodes();
+		// Additionally, sort neighbouring cells
+		cell.second->sort_neighbours_by_bearing();
+	}
+
+	// All nodes have full set of connected_nodes now, so sort
+	for (auto& node : known_nodes) {
+		node.second->sort_connected_nodes_by_bearing();
+		node.second->sort_surrounding_cells_by_bearing();
+	}
+
+	// TODO: Calculate and store neighbour node/cell distances and/or proportions during above process
+};
+
+shared_ptr<SpatialCell> World::retrieve_or_create_cell(map<Point const * const, SpatialCell * const>& known_cells, shared_ptr<Point> p) {
+	auto it = known_cells.find(p.get());
+	if (it == known_cells.end()) {
+		// Cell doesn't already exist
+		return make_shared<SpatialCell>(p);
+	}
+	else {
+		return shared_ptr<SpatialCell>{ it->second };
+	}
+};
+
+shared_ptr<SpatialNode> World::retrieve_or_create_node(map<Triangle const * const, SpatialNode * const>& known_nodes, Triangle const * const t) {
+	auto it = known_nodes.find(t);
+	if (it == known_nodes.end()) {
+		// Node doesn't already exist
+		return make_shared<SpatialNode>(t->create_centre_point());
+	}
+	else {
+		return shared_ptr<SpatialNode>{ it->second };
 	}
 };
--- evere++/World.h
+++ evere++/World.h
@@ -3,6 +3,7 @@
 
 #include <vector>
 #include <memory>
+#include <map>
 
 #include "Triangle.h"
 #include "SpatialNode.h"
@@ -13,6 +14,9 @@ public:
 	std::vector<std::unique_ptr<SpatialNode>> nodes;
 private:
 	void construct_nodes(std::vector<std::unique_ptr<Triangle>> const & triangles);
+
+	std::shared_ptr<SpatialCell> retrieve_or_create_cell(std::map<Point const * const, SpatialCell * const>& known_cells, std::shared_ptr<Point> p);
+	std::shared_ptr<SpatialNode> retrieve_or_create_node(std::map<Triangle const * const, SpatialNode * const>& known_nodes, Triangle const * const t);
 };
 
 #endif // WORLD_H
