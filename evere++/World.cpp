451cdc6873ff0f678a748d96dafd132d14f30024
Sam Corcoran <samuel.corcoran@gmail.com>
2018-09-24 22:35:25 +0100

    Added nodegraph construction timing information

--- evere++/World.cpp
+++ evere++/World.cpp
@@ -5,6 +5,7 @@
 #include "World.h"
 #include "Triangle.h"
 #include "GeometryUtils.h"
+#include <chrono>
 
 using namespace std;
 
@@ -12,10 +13,16 @@ World::World(int const subdivisions) {
 	vector<unique_ptr<Triangle>> triangles;
 	GeometryUtils::create_octahedron_triangles(triangles);
 	GeometryUtils::subdivide_triangles(triangles, subdivisions);
-	cout << "\nTotal triangles generated: " << triangles.size();
-	//print_triangles(triangles);
+	cout << endl << "Total triangles generated: " << triangles.size() << endl;
 
+	//GeometryUtils::print_triangles(triangles);
+
+	chrono::high_resolution_clock::time_point t1 = chrono::high_resolution_clock::now();
 	construct_nodes(triangles);
+	chrono::high_resolution_clock::time_point t2 = chrono::high_resolution_clock::now();
+	auto duration_s = chrono::duration_cast<chrono::seconds>(t2 - t1).count();
+	auto duration_ms = chrono::duration_cast<chrono::milliseconds>(t2 - t1).count();
+	cout << "Nodegraph construction took: " << duration_s << "s (or " << duration_ms << "ms)\n";
 };
 
 void World::construct_nodes(vector<unique_ptr<Triangle>> const & triangles) {
